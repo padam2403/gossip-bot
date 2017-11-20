f44eb2d4582329f98e5876cf1c9e249c57d847ba
padam2403 <padamb.jain24@gmail.com>
2017-11-20 15:14:23 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+if(isset($_REQUEST['hub_challenge'})){
+$challenge = $_REQUEST['hub_challenge'];
+$token=$_REQUEST['hub_verify_token'];
+}
+if($token=="myCustomToken123"){
+echo $challenge;
+}
